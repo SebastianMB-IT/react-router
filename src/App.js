bbeb17af79ef690323205c08e00750425e674b7d
Sebastian <sebastian.besel@nethesis.it>
2022-05-17 01:27:36 +0200

    Link buttons to URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, useLocation } from 'react-router-dom'
 
 function App() {
+  const location = useLocation()
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +11,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className='buttons'>
+          <Link to='/first' className={`button ${location.pathname === '/first' ? 'active' : ''}`}>
+            First
+          </Link>
+          <Link to='/second' className={`button ${location.pathname === '/second' ? 'active' : ''}`}>
+            Second
+          </Link>
+          <Link to='/third' className={`button ${location.pathname === '/third' ? 'active' : ''}`}>
+            Third
+          </Link>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
